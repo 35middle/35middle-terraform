abaaf3fc9138874268ce6f067e8db83edb0a2664
Leo Liu <leo.liu@domain.com.au>
2022-08-05 14:30:51 +1000

    feat(vpc): add new VPC and add one subnet on ap-southeast-2a

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
 provider "aws" {
   region = "ap-southeast-2"
+
+}
+
+variable "vpc_cidr_block" {}
+
+variable "subnet_cidr_block" {}
+
+variable "avail_zone" {}
+
+variable "env_prefix" {}
+
+resource "aws_vpc" "iv_vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name = "${var.env_prefix}-iv-vpc"
+    }
+}
+
+resource "aws_subnet" "iv-subnet-1" {
+  vpc_id = aws_vpc.iv_vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name = "${var.env_prefix}-iv-subnet-1"
+  }
 }
