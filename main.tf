d0f9a4af194aa37f8acb93d24bbaf85a16fd13f3
Leo Liu <leo.liu@domain.com.au>
2022-08-05 14:54:13 +1000

    feat(vpc): use default route table and associate subnet

--- main.tf
+++ main.tf
@@ -27,17 +27,6 @@ resource "aws_subnet" "iv-subnet-1" {
   }
 }
 
-resource "aws_route_table" "iv_route_table" {
-  vpc_id = aws_vpc.iv_vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.iv_igw.id
-  }
-  tags = {
-    Name = "${var.env_prefix}-iv-route-table"
-  }
-}
-
 resource "aws_internet_gateway" "iv_igw" {
   vpc_id = aws_vpc.iv_vpc.id
   tags = {
@@ -45,7 +34,19 @@ resource "aws_internet_gateway" "iv_igw" {
   }
 }
 
+resource "aws_default_route_table" "iv_default_route_table" {
+  default_route_table_id = aws_vpc.iv_vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.iv_igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-iv-default-route-table"
+  }
+}
+
 resource "aws_route_table_association" "iv_rt_assoc" {
-  route_table_id = aws_route_table.iv_route_table.id
+  route_table_id = aws_default_route_table.iv_default_route_table.id
   subnet_id = aws_subnet.iv-subnet-1.id
 }
