9be2da82e33d40dc808149ee5d7da238f6a95262
Leo Liu <leo.liu@domain.com.au>
2022-08-09 18:14:12 +1000

    feat(iv-app): setup infra

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,106 @@ provider "aws" {
 locals {
   # Target port to expose
   target_port = 3000
+  ## ECS Service config
+  ecs_launch_type = "FARGATE"
+  ecs_desired_count = 2
+  ecs_network_mode = "awsvpc"
+  ecs_cpu = 512
+  ecs_memory = 1024
+  ecs_container_name = "iv-app-image"
+  ecs_log_group = "/aws/ecs/${var.project_id}-${var.env}"
+  # Retention in days
+  ecs_log_retention = 1
+}
+
+data "template_file" "task_def_generated" {
+  template = file("./task-definitions/service.json.tpl")
+  vars = {
+    env                 = var.env
+    port                = local.target_port
+    name                = local.ecs_container_name
+    cpu                 = local.ecs_cpu
+    memory              = local.ecs_memory
+    aws_region          = var.aws_region
+    ecs_execution_role  = module.ecs_roles.ecs_execution_role_arn
+    launch_type         = local.ecs_launch_type
+    network_mode        = local.ecs_network_mode
+    log_group           = local.ecs_log_group
+  }
+}
+
+resource "local_file" "output_task_def" {
+  content         = data.template_file.task_def_generated.rendered
+  file_permission = "644"
+  filename        = "./task-definitions/service.latest.json"
+}
+
+resource "aws_ecs_cluster" "iv_app_cluster" {
+  name = "web-cluster-${var.project_id}-${var.env}"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_ecs_task_definition" "iv-app-task-definition" {
+  family                   = "task-definition-iv-app"
+  execution_role_arn       = module.ecs_roles.ecs_execution_role_arn
+  task_role_arn            = module.ecs_roles.ecs_task_role_arn
+
+  requires_compatibilities = [local.ecs_launch_type]
+  network_mode             = local.ecs_network_mode
+  cpu                      = local.ecs_cpu
+  memory                   = local.ecs_memory
+  container_definitions    = jsonencode(
+    jsondecode(
+      data.template_file.task_def_generated.rendered
+    ).containerDefinitions
+  )
+}
+
+resource "aws_ecs_service" "iv_app_ecs_service" {
+  name            = "web-service-${var.project_id}-${var.env}"
+  cluster         = aws_ecs_cluster.iv_app_cluster.id
+  task_definition = aws_ecs_task_definition.iv-app-task-definition.arn
+  desired_count   = local.ecs_desired_count
+  launch_type = local.ecs_launch_type
+
+  load_balancer {
+    target_group_arn = module.ecs_tg.tg.arn
+    container_name   = local.ecs_container_name
+    container_port   = local.target_port
+  }
+
+  network_configuration {
+    subnets         = module.networking.private_subnets[*].id
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  tags = {
+    Name = "web-service-${var.project_id}-${var.env}"
+  }
+
+  depends_on = [
+    module.alb.lb,
+    module.ecs_tg.tg
+  ]
+}
+
+resource "aws_cloudwatch_log_group" "ecs" {
+  name = local.ecs_log_group
+  # This can be changed
+  retention_in_days = local.ecs_log_retention
+}
+
+## Execution role and task roles
+module "ecs_roles" {
+  source                    = "github.com/Jareechang/tf-modules//iam/ecs?ref=v1.0.1"
+  create_ecs_execution_role = true
+  create_ecs_task_role      = true
+
+  # Extend baseline policy statements (ignore for now)
+  ecs_execution_policies_extension = {}
 }
 
 module "networking" {
