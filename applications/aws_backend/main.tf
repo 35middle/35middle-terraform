d7bc96688c56b81457c291a106a2c06a109f8ecf
thomas ni <857840959@QQ.COM>
2022-11-01 21:51:03 +1100

    terraform fmt

--- applications/aws_backend/main.tf
+++ applications/aws_backend/main.tf
@@ -12,17 +12,17 @@ provider "aws" {
 
 
 module "aws_backend" {
-  source             = "../../modules/aws_backend"
-  prefix             = "35middle"
-  vpc_cidr           = "10.0.0.0/16"
-  availability_zones = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
-  ecr_image_name     = "35middle-backend-image"
-  az_count="3"
-  environment = "uat"
-  app_port = "3001"
+  source                       = "../../modules/aws_backend"
+  prefix                       = "35middle"
+  vpc_cidr                     = "10.0.0.0/16"
+  availability_zones           = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
+  ecr_image_name               = "35middle-backend-image"
+  az_count                     = "3"
+  environment                  = "uat"
+  app_port                     = "3001"
   ecs_task_execution_role_name = "myEcsTaskExecutionRole"
-  fargate_cpu = "1024"
-  fargate_memory = "2048"
-  app_count = "1"
-  health_check_path = "/"
+  fargate_cpu                  = "1024"
+  fargate_memory               = "2048"
+  app_count                    = "1"
+  health_check_path            = "/"
 }
